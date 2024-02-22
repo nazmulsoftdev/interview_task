a3244680228578ea9b39f0dda02a34f4bce84792
nazmulsoftdev <nazmulislamsoftdev@gmail.com>
2024-02-22 19:40:31 +0600

    added github link

--- src/components/reuseable/Logo.jsx
+++ src/components/reuseable/Logo.jsx
@@ -4,7 +4,7 @@ import { FaGithub as GithubLogo } from "react-icons/fa6";
 function Logo() {
   return (
     <div className="absolute top-2">
-      <a href="#" target="_blank">
+      <a href="https://interview-task-sandy.vercel.app/" target="_blank">
         <GithubLogo size={50} color="white" className="cursor-pointer" />
       </a>
     </div>
